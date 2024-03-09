991d849818f7d9e53dddee75dd5415d4a7679258
sabrina <sabrinaahmednitu@gmail.com>
2024-03-10 03:12:35 +0600

    banner done

--- src/Components/Home/Banner/Banner.jsx
+++ src/Components/Home/Banner/Banner.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import './Banner.css' 
+import './Banner.css';
+import apartments from '../../../assets/image/appartments-banner.jpg';
 
 const Banner = () => {
-    return (
-        <div>
-            <h2>i am banner</h2>
+  return (
+    <div>
+      <div className="apartment-banner">
+        <img src={apartments} alt="" />
+        <div className="apartment-banner-text">
+          <h1>Discover Your New Home</h1>
+          <p>Helping 100 million renters find their perfect fit.</p>
+          <div className="search-bar">
+            <input type="text" placeholder='Search by location or point of interest' /> <button>Search</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
