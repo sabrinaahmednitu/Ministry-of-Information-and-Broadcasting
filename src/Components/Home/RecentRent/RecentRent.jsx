0d581e44bcea6aea104479874e981691e48a6845
sabrina <sabrinaahmednitu@gmail.com>
2024-03-12 11:43:12 +0600

    recent cards

--- src/Components/Home/RecentRent/RecentRent.jsx
+++ src/Components/Home/RecentRent/RecentRent.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import './RecentRent.css' 
+import './RecentRent.css';
+import photo1 from '../../../assets/image/ParkLine-apartment-in-Miami-FL.jpg';
 
 const RecentRent = () => {
     return (
-        <div>
-            <h2>i am recentrent</h2>
+      <div>
+        <div className='rent-cards'>
+          <h1>Explore Rentals in Chicago,IL</h1>
+          <div className="rents-container">
+            <div className="rent-apartment">
+              <img src={photo1} alt="" />
+              <div className="rent-apartment-text">
+                <h2>Presidential Towers</h2>
+                <h3>555 W Madison St, Chicago, IL 60661</h3>
+                <h4>Studio - 2 Beds | $1,580 - $6,385</h4>
+              </div>
+            </div>
+            <div className="rent-apartment">
+              <img src={photo1} alt="" />
+              <h2>Presidential Towers</h2>
+              <h3>555 W Madison St, Chicago, IL 60661</h3>
+              <h4>Studio - 2 Beds | $1,580 - $6,385</h4>
+            </div>
+            <div className="rent-apartment">
+              <img src={photo1} alt="" />
+              <h2>Presidential Towers</h2>
+              <h3>555 W Madison St, Chicago, IL 60661</h3>
+              <h4>Studio - 2 Beds | $1,580 - $6,385</h4>
+            </div>
+            <div className="rent-apartment">
+              <img src={photo1} alt="" />
+              <h2>Presidential Towers</h2>
+              <h3>555 W Madison St, Chicago, IL 60661</h3>
+              <h4>Studio - 2 Beds | $1,580 - $6,385</h4>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
