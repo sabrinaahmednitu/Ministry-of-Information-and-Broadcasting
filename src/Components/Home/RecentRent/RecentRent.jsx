e0cdbceef0171aa646115f273d7fe813480c7c93
sabrina <sabrinaahmednitu@gmail.com>
2024-03-15 13:22:53 +0600

    recent cards button

--- src/Components/Home/RecentRent/RecentRent.jsx
+++ src/Components/Home/RecentRent/RecentRent.jsx
@@ -5,7 +5,7 @@ import photo1 from '../../../assets/image/ParkLine-apartment-in-Miami-FL.jpg';
 const RecentRent = () => {
     return (
       <div>
-        <div className='container mx-auto rent-cards'>
+        <div className="container mx-auto rent-cards">
           <h1>Explore Rentals in Chicago,IL</h1>
           <div className="rents-container">
             <div className="rent-apartment">
@@ -34,6 +34,9 @@ const RecentRent = () => {
               <h3>555 W Madison St, Chicago, IL 60661</h3>
               <h4>Studio - 2 Beds | $1,580 - $6,385</h4>
             </div>
+          </div>
+          <div className="recent-button">
+            <button>View More</button>
           </div>
         </div>
       </div>
