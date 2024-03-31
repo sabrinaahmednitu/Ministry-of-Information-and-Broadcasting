b22e3df4a9312bbda5d502ceec420fa6700f2019
sabrina <sabrinaahmednitu@gmail.com>
2024-03-31 23:15:59 +0600

    perfect place done

---
+++ src/Components/RentCards/AddressCard/AddressCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AddressCard = ({ address }) => {
+  return (
+    <div className="address-card">
+      <h2>{address.name}</h2>
+      <p>{address.street}</p>
+      <p>
+        {address.city}, {address.state} {address.zip}
+      </p>
+    </div>
+  );
+};
+
+export default AddressCard;
