f37d998a3dde51398e2880253e94a4aeb87103ad
sabrina <sabrinaahmednitu@gmail.com>
2024-08-16 13:22:16 +0600

    initial commit

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -23,6 +23,9 @@ const Footer = () => {
         <h6 className="footer-title">যোগাযোগ</h6>
       </nav>
       <nav>
+        <h6 className="footer-title">communication</h6>
+      </nav>
+      <nav>
         <h6 className="footer-title">ব্যবহারের শর্তাবলি</h6>
       </nav>
       <nav>
